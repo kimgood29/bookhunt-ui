f9b51e1b9c458f23fedbdd032a62977b589c128c
Joeun Kim <kimgood91@gmail.com>
2020-02-25 17:54:15 +0900

    fix: storybook 타입스크립트(.tsx)로 세팅

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,26 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
   addons: [
+    '@storybook/preset-typescript',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register', // props를 변경할 수 있는 UI 컨트롤 제공
     '@storybook/addon-viewport/register', // phone, tablet, pc 해상도 테스트
+    '@storybook/addon-docs/react/preset',
   ],
 };
 
